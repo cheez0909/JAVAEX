2813140caae31540081f6fcd653f52e074508425
Dani <bin0696@naver.com>
2023-12-11 02:19:30 +0900

    스트림 복습 중 - 12월 11일

---
+++ Stream/src/main/java/StreamEx1.java
@@ -0,0 +1,53 @@
+/**
+ * 1. 작성자 : Dani
+ * 2. 작성일 : 12월 11일
+ * 3. Stream 만들기 예제1
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StreamEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("이름3");
+        names.add("이름1");
+        names.add("이름8");
+        names.add("이름4");
+        names.add("이름5");
+
+
+        /**
+         * Collections의 정렬기능
+         */
+        /*
+        Collections.sort(names);
+        System.out.println(names);
+        */
+
+        /**
+         * 정렬 후 새로운 ArrayList로 만들기
+         */
+        ArrayList<String> collect = names.stream().sorted().collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(collect);
+
+        /**
+         * 정렬 후 새로운 String 배열로 만들기
+         */
+        String[] strings = {"이름1", "이름3","이름9","이름00","이름"};
+        String[] array = Arrays.stream(strings).sorted().toArray(String[]::new);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("============================");
+
+        int total = 0;
+        for(int i=1; i<=100; i++){
+            total += i;
+        }
+        System.out.println("1부터 100 까지의 합 : " + total);
+
+        int total2 = IntStream.rangeClosed(1, 100).sum();
+        System.out.println("1부터 100 까지의 합 : " + total2);
+    }
+}
---
+++ Stream/src/main/java/Student.java
@@ -0,0 +1,40 @@
+public class Student implements Comparable<Student>{
+    private String name;
+    private int ban;
+    private int totalScore;
+
+    public Student(String name, int ban, int totalScore) {
+        this.name = name;
+        this.ban = ban;
+        this.totalScore = totalScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBan() {
+        return ban;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", ban=" + ban +
+                ", totalScore=" + totalScore +
+                '}';
+    }
+
+    /**
+     * 성적이 높은 순으로 정렬
+     */
+    @Override
+    public int compareTo(Student o) {
+        return  o.totalScore - this.totalScore;
+    }
+}
