befe3639e3d99c7fb3d028f8ce548d346746a1ad
dani <bin0696@naver.com>
2023-12-15 17:04:06 +0900

    12월 15일

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 [3. 열거형](https://github.com/cheez0909/JAVAEX/tree/main/Enums)<br>
 [4. 애너테이션](https://github.com/cheez0909/JAVAEX/tree/main/Annotation)<br>
 [5. 지네릭스](https://github.com/cheez0909/JAVAEX/tree/main/Generics)<br>
-
+[6. 람다식](https://github.com/cheez0909/JAVAEX/tree/main/Lamda)<br>
--- Stream/src/main/java/classEx.java
+++ Stream/src/main/java/classEx.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 
 public class classEx {
@@ -19,5 +20,21 @@ public class classEx {
         int sum = IntStream.rangeClosed(1, 100).sum();
         System.out.println(sum);
 
+        List<String> words1 = Arrays.asList("aa", "bb", "cc"); // 배열을 리스트로 만들어줌
+//        words1.stream().map(x->x.toUpperCase()).forEach(System.out::println);
+        List<String> map = map(words1, s -> s.toUpperCase()).stream().toList();
+        System.out.println(map);
+
+    }
+
+    // UnaryOperator : 입력값과 출력값의 타입이 동일하다.
+    // Function  : 입력값과 출력값의 타입이 다를 수 있음.
+    public static List<String> map(List<String> words, UnaryOperator<String> oper){
+        List<String> converted = new ArrayList<>();
+        for(String word : words){
+                String word2 = oper.apply(word);
+                converted.add(word2);
+        }
+        return converted;
     }
 }
